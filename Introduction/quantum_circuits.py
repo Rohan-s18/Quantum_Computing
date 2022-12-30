8a2f1478e9bbdcfc458f38d23a76fafd18c8daa7
Rohan Singh <68778986+Rohan-s18@users.noreply.github.com>
2022-12-30 04:26:35 +0000

    Added Script to intialize the Quantum Circuit

--- Introduction/quantum_circuits.py
+++ Introduction/quantum_circuits.py
@@ -8,4 +8,22 @@ This Python module contains source code simple Quantum Circuits
 from qiskit import QuantumCircuit, assemble, Aer
 from qiskit.visualization import plot_histogram
 
+#Creating a Quantum Circuit with 8 qubits (8 outputs)
+qc_output = QuantumCircuit(8)
+
+#Adding measurements to each of the qubits
+qc_output.measure_all()
+
+#The qubits are all intialized to 0, so the output will be all zeros
+print(qc_output.draw(initial_state=True))
+
+
+
+sim = Aer.get_backend('aer_simulator') 
+result = sim.run(qc_output).result()
+counts = result.get_counts()
+
+print(counts)
+plot_histogram(counts)
+
 
